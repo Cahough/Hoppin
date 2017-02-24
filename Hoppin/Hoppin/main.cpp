4e686215e624c6b9741a1b4103b6625caa6797ee
Chough <carter.hough@hotmail.com>
2017-02-24 09:29:45 -0700

    Initial Commit

---
+++ Hoppin/Hoppin/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Hoppin
+//
+//  Created by Carter Hough on 2/24/17.
+//  Copyright © 2017 Carter Hough, Ben Hanberry. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
